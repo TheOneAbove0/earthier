18c04c23e395fbd8452b12f39ae9a64bf791cf37
TheOneAbove0 <manishthapaliya8888@gmail.com>
2023-10-18 17:48:34 +0545

    added users tables

---
+++ src/components/Dashboard/Users/UsersTable/TableBody.jsx
@@ -0,0 +1,19 @@
+import { Table } from "../../../Common/Table/Table";
+import { TableData, columns, renderRow } from "./TableData";
+
+export default function TableBody() {
+  const data = TableData;
+
+
+
+  const itemsPerPage = 5;
+
+  return (
+    <Table
+      data={data}
+      itemsPerPage={itemsPerPage}
+      columns={columns}
+      renderRow={renderRow}
+    />
+  );
+}
---
+++ src/components/Dashboard/Users/UsersTable/TableData.jsx
@@ -0,0 +1,526 @@
+import { Avatar } from "../../../assets/image";
+import { ThreeDot } from "../../../assets/icons";
+
+export const TableData = [
+  {
+    id: 5,
+    Name: "Darlene Robertson",
+    Email: "ziar@gmail.com",
+    Image: Avatar,
+    DeliveryCollection: {
+      Collection1: {
+        Total: "",
+        Amount: "",
+      },
+      Collection2: {
+        Collection: "",
+        Amount: "",
+      },
+      Collection3: {
+        Return: "",
+        Amount: "",
+      },
+    },
+
+    Role: {
+      Role1: "Owner",
+      Role2: "",
+    },
+    Date: "03/01/21   6:05am",
+  },
+  {
+    id: 4,
+    Name: "Ronald Richards",
+    Email: "dric@gmail.com",
+    Role: {
+      Role1: "Accountant",
+      Role2: "",
+    },
+
+    Image: Avatar,
+    DeliveryCollection: {
+      Collection1: {
+        Total: "",
+        Amount: "",
+      },
+      Collection2: {
+        Collection: "",
+        Amount: "",
+      },
+      Collection3: {
+        Return: "",
+        Amount: "",
+      },
+    },
+    Date: "03/10/21   11:43am",
+  },
+  {
+    id: 3,
+    Name: "Dianne Russell",
+    Email: "cedennar@gmail.com",
+
+    Role: {
+      Role1: "Delivery",
+      Role2: "",
+    },
+    Image: Avatar,
+    DeliveryCollection: {
+      Collection1: {
+        Total: "Total",
+        Amount: 1250,
+      },
+      Collection2: {
+        Collection: "Collection",
+        Amount: 1500,
+      },
+      Collection3: {
+        Return: "Return",
+        Amount: 250,
+      },
+    },
+    Date: "03/10/21   11:43am",
+  },
+
+  {
+    id: 2,
+    Name: "John Doe",
+    Email: "Johndoe@gmail.com",
+    Image: Avatar,
+    Role: {
+      Role1: "Staff",
+      Role2: "Accountant",
+    },
+    DeliveryCollection: {
+      Collection1: {
+        Total: "",
+        Amount: "",
+      },
+      Collection2: {
+        Collection: "",
+        Amount: "",
+      },
+      Collection3: {
+        Return: "",
+        Amount: "",
+      },
+    },
+    Date: "03/07/21   12:27pm",
+  },
+
+  {
+    id: 1,
+    Name: "Theresa Webb",
+    Email: "quasiah@gmail.com",
+
+    Role: {
+      Role1: "Admin",
+      Role2: "",
+    },
+    Image: Avatar,
+    DeliveryCollection: {
+      Collection1: {
+        Total: "",
+        Amount: "",
+      },
+      Collection2: {
+        Collection: "",
+        Amount: "",
+      },
+      Collection3: {
+        Return: "",
+        Amount: "",
+      },
+    },
+    Date: "03/05/21   6:16pm",
+  },
+  {
+    id: 5,
+    Name: "Darlene Robertson",
+    Email: "ziar@gmail.com",
+    Image: Avatar,
+    DeliveryCollection: {
+      Collection1: {
+        Total: "",
+        Amount: "",
+      },
+      Collection2: {
+        Collection: "",
+        Amount: "",
+      },
+      Collection3: {
+        Return: "",
+        Amount: "",
+      },
+    },
+
+    Role: {
+      Role1: "Owner",
+      Role2: "",
+    },
+    Date: "03/01/21   6:05am",
+  },
+  {
+    id: 4,
+    Name: "Ronald Richards",
+    Email: "dric@gmail.com",
+    Role: {
+      Role1: "Accountant",
+      Role2: "",
+    },
+
+    Image: Avatar,
+    DeliveryCollection: {
+      Collection1: {
+        Total: "",
+        Amount: "",
+      },
+      Collection2: {
+        Collection: "",
+        Amount: "",
+      },
+      Collection3: {
+        Return: "",
+        Amount: "",
+      },
+    },
+    Date: "03/10/21   11:43am",
+  },
+  {
+    id: 3,
+    Name: "Dianne Russell",
+    Email: "cedennar@gmail.com",
+
+    Role: {
+      Role1: "Delivery",
+      Role2: "",
+    },
+    Image: Avatar,
+    DeliveryCollection: {
+      Collection1: {
+        Total: "Total",
+        Amount: 1250,
+      },
+      Collection2: {
+        Collection: "Collection",
+        Amount: 1500,
+      },
+      Collection3: {
+        Return: "Return",
+        Amount: 250,
+      },
+    },
+    Date: "03/10/21   11:43am",
+  },
+
+  {
+    id: 2,
+    Name: "John Doe",
+    Email: "Johndoe@gmail.com",
+    Image: Avatar,
+    Role: {
+      Role1: "Staff",
+      Role2: "Accountant",
+    },
+    DeliveryCollection: {
+      Collection1: {
+        Total: "",
+        Amount: "",
+      },
+      Collection2: {
+        Collection: "",
+        Amount: "",
+      },
+      Collection3: {
+        Return: "",
+        Amount: "",
+      },
+    },
+    Date: "03/07/21   12:27pm",
+  },
+
+  {
+    id: 1,
+    Name: "Theresa Webb",
+    Email: "quasiah@gmail.com",
+
+    Role: {
+      Role1: "Admin",
+      Role2: "",
+    },
+    Image: Avatar,
+    DeliveryCollection: {
+      Collection1: {
+        Total: "",
+        Amount: "",
+      },
+      Collection2: {
+        Collection: "",
+        Amount: "",
+      },
+      Collection3: {
+        Return: "",
+        Amount: "",
+      },
+    },
+    Date: "03/05/21   6:16pm",
+  },
+  {
+    id: 5,
+    Name: "Darlene Robertson",
+    Email: "ziar@gmail.com",
+    Image: Avatar,
+    DeliveryCollection: {
+      Collection1: {
+        Total: "",
+        Amount: "",
+      },
+      Collection2: {
+        Collection: "",
+        Amount: "",
+      },
+      Collection3: {
+        Return: "",
+        Amount: "",
+      },
+    },
+
+    Role: {
+      Role1: "Owner",
+      Role2: "",
+    },
+    Date: "03/01/21   6:05am",
+  },
+  {
+    id: 4,
+    Name: "Ronald Richards",
+    Email: "dric@gmail.com",
+    Role: {
+      Role1: "Accountant",
+      Role2: "",
+    },
+
+    Image: Avatar,
+    DeliveryCollection: {
+      Collection1: {
+        Total: "",
+        Amount: "",
+      },
+      Collection2: {
+        Collection: "",
+        Amount: "",
+      },
+      Collection3: {
+        Return: "",
+        Amount: "",
+      },
+    },
+    Date: "03/10/21   11:43am",
+  },
+  {
+    id: 3,
+    Name: "Dianne Russell",
+    Email: "cedennar@gmail.com",
+
+    Role: {
+      Role1: "Delivery",
+      Role2: "",
+    },
+    Image: Avatar,
+    DeliveryCollection: {
+      Collection1: {
+        Total: "Total",
+        Amount: 1250,
+      },
+      Collection2: {
+        Collection: "Collection",
+        Amount: 1500,
+      },
+      Collection3: {
+        Return: "Return",
+        Amount: 250,
+      },
+    },
+    Date: "03/10/21   11:43am",
+  },
+
+  {
+    id: 2,
+    Name: "John Doe",
+    Email: "Johndoe@gmail.com",
+    Image: Avatar,
+    Role: {
+      Role1: "Staff",
+      Role2: "Accountant",
+    },
+    DeliveryCollection: {
+      Collection1: {
+        Total: "",
+        Amount: "",
+      },
+      Collection2: {
+        Collection: "",
+        Amount: "",
+      },
+      Collection3: {
+        Return: "",
+        Amount: "",
+      },
+    },
+    Date: "03/07/21   12:27pm",
+  },
+
+  {
+    id: 1,
+    Name: "Theresa Webb",
+    Email: "quasiah@gmail.com",
+
+    Role: {
+      Role1: "Admin",
+      Role2: "",
+    },
+    Image: Avatar,
+    DeliveryCollection: {
+      Collection1: {
+        Total: "",
+        Amount: "",
+      },
+      Collection2: {
+        Collection: "",
+        Amount: "",
+      },
+      Collection3: {
+        Return: "",
+        Amount: "",
+      },
+    },
+    Date: "03/05/21   6:16pm",
+  },
+];
+
+export const RoleColors = {
+  Owner: "bg-Success bg-opacity-10 text-Success",
+  Accountant: "bg-LightBlue bg-opacity-10 text-LightBlue",
+  Delivery: "bg-Black bg-opacity-10 text-Black",
+  Staff: "bg-BlueColor bg-opacity-10 text-BlueColor",
+  Admin: "bg-Black bg-opacity-10 ",
+};
+
+export const DeliveryCollection = {
+    Total: "bg-AdminPanel  text-Black",
+    Collection: "bg-Success bg-opacity-10 text-Success",
+    Return: "bg-Danger bg-opacity-10 text-Danger",
+};
+
+export const columns = [
+  {
+    header: <input className="w-4 h-4 rounded-[4px]" type="checkbox" />,
+    className: "px-3 whitespace-nowrap pt-4 pb-2 w-[64px]",
+  },
+  {
+    header: "#",
+    className:
+      "pl-2 text-start text-xs font-medium  leading-[16px] whitespace-nowrap pt-4 pb-2 w-[64px]",
+  },
+  {
+    header: "Name",
+    className:
+      "pl-4 text-start text-xs font-medium leading-[16px] whitespace-nowrap pt-4 pb-2 w-[50%]",
+  },
+  {
+    header: "Role",
+    className:
+      "pl-2 text-start text-xs font-medium leading-[16px] whitespace-nowrap pt-4 pb-2 w-[200px]",
+  },
+  {
+    header: "Delivery collection",
+    className:
+      "pl-2 text-start text-xs font-medium leading-[16px] pt-4 whitespace-nowrap pb-2 w-[320px]",
+  },
+  {
+    header: "Created at",
+    className:
+      "pl-2 text-start text-xs font-medium leading-[16px] pt-4 whitespace-nowrap pb-2 w-[160px]",
+  },
+  {
+    header: "Action",
+    className:
+      "pl-[11px] text-xs font-medium leading-[16px] text-start pt-4 pr-2 whitespace-nowrap pb-2 w-[56px]",
+  },
+];
+
+export const renderRow = (item, index) => (
+  <tr key={index}>
+    <td className="px-6 whitespace-nowrap pt-4 pb-2 w-[64px]">
+      <input className="w-4 h-4 rounded-[4px]" type="checkbox" />
+    </td>
+
+    <td className="pl-2 whitespace-nowrap pt-2 pb-2 w-[64px]">
+      <div className="font-medium text-xs leading-[16px] text-start ">
+        {item.id}
+      </div>
+    </td>
+
+    <td className="pl-4 whitespace-nowrap  py-[15px] w-[40%]">
+      <div className=" flex gap-2 ">
+        <img className=" object-cover " src={item.Image} alt="Avatar" />
+        <div className=" flex flex-col items-start justify-start ">
+          <span className=" text-[13px] leading-[18px] tracking-[-0.08px] ">
+            {item.Name}
+          </span>
+          <span className=" text-[12px] leading-[16px] text-Tertiary">
+            {item.Email}
+          </span>
+        </div>
+      </div>
+    </td>
+
+    <td className="px-2 py-5 whitespace-nowrap w-[80px]">
+      <div className="text-xs  flex gap-2  leading-[16px]">
+        <span className={` py-1 px-2 ${RoleColors[item.Role.Role1] || ""} `}>
+          {item.Role.Role1}
+        </span>
+        <span className={` py-1 px-2 ${RoleColors[item.Role.Role2] || ""} `}>{item.Role.Role2}</span>
+      </div>
+    </td>
+    <td className="py-2 whitespace-nowrap px-2 w-[120px]">
+      <div className="text-xs  flex w-full gap-2 leading-[16px]">
+        <div className={`flex flex-col rounded-lg gap-1 px-2 py-2 w-full ${DeliveryCollection[item.DeliveryCollection.Collection1.Total] || ""} `}  >
+            <span>
+            {item.DeliveryCollection.Collection1.Total}
+            </span>
+            <span>
+            {item.DeliveryCollection.Collection1.Amount}
+            </span>
+        </div>
+        <div className={`flex flex-col rounded-lg gap-1 px-2 py-2 w-full ${DeliveryCollection[item.DeliveryCollection.Collection2.Collection] || ""} `} >
+            <span>
+            {item.DeliveryCollection.Collection2.Collection}
+            </span>
+            <span>
+            {item.DeliveryCollection.Collection2.Amount}
+            </span>
+        </div>
+        <div className={`flex flex-col rounded-lg gap-1 px-2 py-2 w-full ${DeliveryCollection[item.DeliveryCollection.Collection3.Return] || ""} `} >
+            <span>
+            {item.DeliveryCollection.Collection3.Return}
+            </span>
+            <span>
+            {item.DeliveryCollection.Collection3.Amount}
+            </span>
+        </div>
+       
+      </div>
+    </td>
+    <td className={`py-6  whitespace-nowrap  w-[106px]`}>
+      <div className={`text-xs w-fit rounded-[3px] px-2 leading-[16px]  `}>
+        {item.Date}
+      </div>
+    </td>
+
+    <td className="pl-[28px] py-[10px]  whitespace-nowrap w-[56px]">
+      <div className="font-medium text-xs leading-[16px] text-start">
+        <img src={ThreeDot} alt="ThreeDot" />
+      </div>
+    </td>
+  </tr>
+);
