676a35d896f31874710637f6e03112378f67b682
TheOneAbove0 <manishthapaliya8888@gmail.com>
2023-10-19 08:04:35 +0545

    finalize create role

--- src/components/Dashboard/Users/CreateRole/Allowed.jsx
+++ src/components/Dashboard/Users/CreateRole/Allowed.jsx
@@ -1,78 +1,111 @@
-import React from 'react';
-import { chevrondown, searchBlue } from '../../../assets/icons';
+import React from "react";
+import { chevrondown, searchBlue } from "../../../assets/icons";
 
-export default function Allowed({ checkedItems, clickedItemText, categories, handleCheckboxChange }) {
+export default function Allowed({
+  checkedItems,
+  categories,
+  handleCheckboxChange,
+}) {
   return (
-    <div className='flex w-full flex-col gap-4'>
-      <div className='flex items-center justify between'>
-        <span className='text-[15px] leading-[20px] tracking-[-0.5px] font-bold'>Allowed</span>
-        <span className='text-[12px] leading-[16px] text-Danger'>Clear permissions</span>
+    <div className="flex w-full flex-col gap-4">
+      <div className="flex items-center w-full justify-between">
+        <span className="text-[15px] leading-[20px] tracking-[-0.5px] font-bold">
+          Allowed
+        </span>
+        <span className="text-[12px] leading-[16px] text-Danger">
+          Clear permissions
+        </span>
       </div>
-      <div className='rounded-2xl w-full p-4 flex flex-col gap-2 bg-LightGrey'>
-        <div className='flex justify-between px-2 bg-white w-full items-center'>
-          <input className='focus:outline-none text-[13px] leading-[18px] tracking-[-0.08px] py-[7px]' type="text" placeholder='Search' />
+      <div className="rounded-2xl w-full h-full p-4  flex flex-col gap-2 bg-LightGrey">
+        <div className="flex justify-between px-2 bg-white w-full items-center">
+          <input
+            className="focus:outline-none w-full text-[13px] leading-[18px] tracking-[-0.08px] py-[7px]"
+            type="text"
+            placeholder="Search"
+          />
           <img src={searchBlue} alt="searchBlue" />
         </div>
 
-        {/* Display the text of the clicked item when a checkbox is clicked */}
-        {clickedItemText && <p>Clicked item: {clickedItemText}</p>}
-
         {categories.map((category, categoryIndex) => (
           <div key={categoryIndex}>
-            <div className='flex items-center pl-2 gap-4'>
-              <img src={chevrondown} alt='chevrondown' />
-              <span className='font-bold py-2 leading-[18px] text-[13px] tracking-[-0.08px]'>
-                {category.name}
-              </span>
-            </div>
-            <div className='flex flex-col w-full pl-8'>
-              <div className='flex'>
-                <div className='flex items-center w-full px-2 gap-2'>
-                  <input
-                    className='w-3 h-3'
-                    type='checkbox'
-                    checked={checkedItems[categoryIndex].actions}
-                    onChange={() => handleCheckboxChange(categoryIndex, 'actions')}
-                  />
-                  <span className='text-[13px] leading-[18px] tracking-[-0.08px] py-1'>
-                    {category.actions}
-                  </span>
-                </div>
-                <div className='flex items-center w-full px-2 gap-2'>
-                  <input
-                    className='w-3 h-3'
-                    type='checkbox'
-                    checked={checkedItems[categoryIndex].create}
-                    onChange={() => handleCheckboxChange(categoryIndex, 'create')}
-                  />
-                  <span className='text-[13px] leading-[18px] tracking-[-0.08px] py-1'>
-                    {category.create}
-                  </span>
-                </div>
+            {checkedItems[categoryIndex].actions ||
+            checkedItems[categoryIndex].create ||
+            checkedItems[categoryIndex].permission ||
+            checkedItems[categoryIndex].delete ? (
+              <div className="flex items-center pl-2  gap-4">
+                <img src={chevrondown} alt="chevrondown" />
+                <span className="font-bold py-2 leading-[18px] text-[13px] tracking-[-0.08px]">
+                  {category.name}
+                </span>
               </div>
-              <div className='flex'>
-                <div className='flex items-center w-full px-2 gap-2'>
-                  <input
-                    className='w-3 h-3'
-                    type='checkbox'
-                    checked={checkedItems[categoryIndex].permission}
-                    onChange={() => handleCheckboxChange(categoryIndex, 'permission')}
-                  />
-                  <span className='text-[13px] leading-[18px] tracking-[-0.08px] py-1'>
-                    {category.permission}
-                  </span>
-                </div>
-                <div className='flex items-center w-full px-2 gap-2'>
-                  <input
-                    className='w-3 h-3'
-                    type='checkbox'
-                    checked={checkedItems[categoryIndex].delete}
-                    onChange={() => handleCheckboxChange(categoryIndex, 'delete')}
-                  />
-                  <span className='text-[13px] leading-[18px] tracking-[-0.08px] py-1'>
-                    {category.delete}
-                  </span>
-                </div>
+            ) : (
+              ""
+            )}
+
+            <div className="flex flex-col w-full pl-8">
+              <div className="flex">
+                {checkedItems[categoryIndex].actions && (
+                  <div className="flex items-center w-full px-2 gap-2">
+                    <input
+                      className="w-3 h-3"
+                      type="checkbox"
+                      checked={checkedItems[categoryIndex].actions}
+                      onChange={() =>
+                        handleCheckboxChange(categoryIndex, "actions")
+                      }
+                    />
+                    <span className="text-[13px] leading-[18px] tracking-[-0.08px] py-1">
+                      {category.actions}
+                    </span>
+                  </div>
+                )}
+                {checkedItems[categoryIndex].create && (
+                  <div className="flex items-center w-full px-2 gap-2">
+                    <input
+                      className="w-3 h-3"
+                      type="checkbox"
+                      checked={checkedItems[categoryIndex].create}
+                      onChange={() =>
+                        handleCheckboxChange(categoryIndex, "create")
+                      }
+                    />
+                    <span className="text-[13px] leading-[18px] tracking-[-0.08px] py-1">
+                      {category.create}
+                    </span>
+                  </div>
+                )}
+              </div>
+              <div className="flex">
+                {checkedItems[categoryIndex].permission && (
+                  <div className="flex items-center w-full px-2 gap-2">
+                    <input
+                      className="w-3 h-3"
+                      type="checkbox"
+                      checked={checkedItems[categoryIndex].permission}
+                      onChange={() =>
+                        handleCheckboxChange(categoryIndex, "permission")
+                      }
+                    />
+                    <span className="text-[13px] leading-[18px] tracking-[-0.08px] py-1">
+                      {category.permission}
+                    </span>
+                  </div>
+                )}
+                {checkedItems[categoryIndex].delete && (
+                  <div className="flex items-center w-full px-2 gap-2">
+                    <input
+                      className="w-3 h-3"
+                      type="checkbox"
+                      checked={checkedItems[categoryIndex].delete}
+                      onChange={() =>
+                        handleCheckboxChange(categoryIndex, "delete")
+                      }
+                    />
+                    <span className="text-[13px] leading-[18px] tracking-[-0.08px] py-1">
+                      {category.delete}
+                    </span>
+                  </div>
+                )}
               </div>
             </div>
           </div>
--- src/components/Dashboard/Users/CreateRole/Main.jsx
+++ src/components/Dashboard/Users/CreateRole/Main.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import Permissions from './Permissions';
-import Allowed from './Allowed';
-import { categories } from './Data';
+import React, { useState } from "react";
+import Permissions from "./Permissions";
+import Allowed from "./Allowed";
+import { categories } from "./Data";
 
 export default function Main() {
   const initialCheckedItems = categories.map(() => ({
@@ -12,13 +12,13 @@ export default function Main() {
   }));
 
   const [checkedItems, setCheckedItems] = useState(initialCheckedItems);
-  const [clickedItemText, setClickedItemText] = useState('');
+  const [clickedItemText, setClickedItemText] = useState("");
 
   const handleCheckboxChange = (categoryIndex, permissionType) => {
     const newCheckedItems = [...checkedItems];
-    newCheckedItems[categoryIndex][permissionType] = !newCheckedItems[categoryIndex][permissionType];
+    newCheckedItems[categoryIndex][permissionType] =
+      !newCheckedItems[categoryIndex][permissionType];
 
-    // Set the clickedItemText based on the permission type
     const clickedItemText = categories[categoryIndex][permissionType];
 
     setCheckedItems(newCheckedItems);
@@ -26,15 +26,32 @@ export default function Main() {
   };
 
   return (
-    <div className='flex w-full bg-white px-4 flex-col py-4 gap-4'>
-    <div className='flex flex-col gap-2'>
-      <div className='text-xs leading-[16px]'>Role <span className='text-[11px] leading-[13px] tracking-[0.07px] text-Danger'>*</span></div>
-      <input type='text' placeholder='Eg: Worker' className='w-full p-4 text-xs leading-[16px] focus:outline-none bg-LightGrey rounded-lg' />
+    <div className="flex w-full bg-white px-4 flex-col py-4 gap-4">
+      <div className="flex flex-col gap-2">
+        <div className="text-xs leading-[16px]">
+          Role{" "}
+          <span className="text-[11px] leading-[13px] tracking-[0.07px] text-Danger">
+            *
+          </span>
+        </div>
+        <input
+          type="text"
+          placeholder="Eg: Worker"
+          className="w-full p-4 text-xs leading-[16px] focus:outline-none bg-LightGrey rounded-lg"
+        />
+      </div>
+      <div className="flex  w-full p-4 gap-4">
+        <Permissions
+          categories={categories}
+          checkedItems={checkedItems}
+          handleCheckboxChange={handleCheckboxChange}
+        />
+        <Allowed
+          categories={categories}
+          checkedItems={checkedItems}
+          clickedItemText={clickedItemText}
+        />
+      </div>
     </div>
-    <div className='flex w-full p-4 gap-4'>
-      <Permissions categories={categories} checkedItems={checkedItems} handleCheckboxChange={handleCheckboxChange} />
-      <Allowed categories={categories} checkedItems={checkedItems} clickedItemText={clickedItemText} />
-    </div>
-  </div>
   );
 }
